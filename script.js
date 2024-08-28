b8bedbfa131211664cf9a12d4564b7305d34b5df
Slippy <116444403+slippyishappy@users.noreply.github.com>
2024-08-28 11:11:36 +0100

    Fixed some styling issues

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const menu = document.querySelector("#menu");
+const menuClose = document.querySelector("#menu-close");
 const tabs = document.querySelector("#tabs");
 
 if (window.innerWidth <= 542) {
     tabs.classList.remove("active");
     tabs.classList.add("hidden");
+    menu.style = "display: block;"
 
     menu.addEventListener("click", () => {
         tabs.classList.toggle("active");        
         tabs.classList.toggle("hidden");
     });
 }
+
+
